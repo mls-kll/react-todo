961ef3773a6b1d8e77dcf71d504d374b720abf7d
mls-kll <milos.kallai@jaystack.com>
2019-07-04 17:01:12 +0200

    default state value changed

--- todo-app/src/reducers/todos.js
+++ todo-app/src/reducers/todos.js
@@ -11,7 +11,7 @@ const defaultState = {
     {
       id: 1,
       title: 'todo 2',
-      isCompleted: false,
+      isCompleted: true,
       description: 'todo 2 description',
       showEdit: false
     },
