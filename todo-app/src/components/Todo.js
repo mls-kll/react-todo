46bf27a7fcf7c5cb16cd8a620ea90e608790e6f8
mls-kll <milos.kallai@jaystack.com>
2019-07-04 13:00:47 +0200

    todo items styled

--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Todo = ({ name, isCompleted, handleComplete, removeTodo }) => {
   return (
-    <li className='list-group-item text-center'>
+    <li className='list-group-item todo-list-item'>
       <span className={isCompleted ? 'complete' : 'uncomplete'} onClick={handleComplete}>{name}</span>
       <button className='btn btn-danger ml-5' onClick={removeTodo}> remove</button>
     </li>
