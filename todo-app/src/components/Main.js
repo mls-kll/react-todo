0af322168434943b19b452be23eb9de5a91f2d07
mls-kll <milos.kallai@jaystack.com>
2019-07-08 13:49:07 +0200

    added search feature

--- todo-app/src/components/Main.js
+++ todo-app/src/components/Main.js
@@ -4,36 +4,59 @@ import Todo from './Todo';
 import { removeTodo, completeTodo } from '../actions/index';
 import { Link } from 'react-router-dom';
 
-const Main = ({ todos, removeTodo, completeTodo }) => {
-  return (
-    <div className="Main">
-      <div className="todo-wrapper border rounded">
-        <div className="m-2">
-          <input type="text" />
-          <i className="fas fa-search ml-2" />
-        </div>
-        <div className="add-todo-icon-container">
-          <Link to="/create">
-            <i className="create-todo fas fa-plus text-primary" />
-          </Link>
-        </div>
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+  }
 
-        <ul className="list-group list-group-flush">
-          {todos.map(todo => (
-            <Todo
-              key={todo.id}
-              id={todo.id}
-              title={todo.title}
-              isCompleted={todo.isCompleted}
-              handleComplete={() => completeTodo(todo.id)}
-              removeTodo={() => removeTodo(todo.id)}
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { todos, removeTodo, completeTodo } = this.props;
+    const { query } = this.state;
+    return (
+      <div className="Main">
+        <div className="todo-wrapper border rounded">
+          <div className="m-2">
+            <input
+              type="text"
+              name="query"
+              onChange={event => this.handleChange(event)}
+              value={query}
             />
-          ))}
-        </ul>
+            <i className="fas fa-search ml-2" />
+          </div>
+          <div className="add-todo-icon-container">
+            <Link to="/create">
+              <i className="create-todo fas fa-plus text-primary" />
+            </Link>
+          </div>
+
+          <ul className="list-group list-group-flush">
+            {todos.map(
+              todo =>
+                todo.title.includes(this.state.query.toLowerCase()) && (
+                  <Todo
+                    key={todo.id}
+                    id={todo.id}
+                    title={todo.title}
+                    isCompleted={todo.isCompleted}
+                    handleComplete={() => completeTodo(todo.id)}
+                    removeTodo={() => removeTodo(todo.id)}
+                  />
+                )
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
