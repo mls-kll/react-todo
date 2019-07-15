a6efb6d470f2704c54927c25316b79dd32c2960d
mls-kll <milos.kallai@jaystack.com>
2019-07-15 17:25:58 +0200

    add autosuggest dependencie

--- server/server.js
+++ server/server.js
@@ -8,24 +8,32 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//parnet id, prenteknek legyenek propertiuk, amik tartalmazzák a childeket
+//körmentes gráf
+//gráfok megnézni
+//mik azok a feladatok, amiknek nincs függőségük
+
 let todos = [
   {
     id: '0ceb1eb9-26d8-4ea8-b831-d99575a78e02',
     title: 'etesd meg a kecskédet',
     description: 'éhes',
-    isCompleted: false
+    isCompleted: false,
+    parentId: null
   },
   {
     id: '75d3208e-c753-4432-ba0b-e7508bea1b07',
     title: 'vegyél sört',
     description: '',
-    isCompleted: false
+    isCompleted: false,
+    parentId: null
   },
   {
     id: '933d86ac-832b-4f02-88ac-54e7414d7241',
     title: 'takaríts ki',
     description: 'kosz van',
-    isCompleted: true
+    isCompleted: true,
+    parentId: null
   }
 ];
 
